a32a91c0915bd09266b2e8a50dfc0964ebcb4d76
Denis Panasyuk <den.dzim@yandex.ru>
2026-04-15 14:22:04 +0300

    checkstyle

--- server/src/test/java/ru/practicum/shareit/request/dao/ItemRequestDtoTest.java
+++ server/src/test/java/ru/practicum/shareit/request/dao/ItemRequestDtoTest.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.request.dao;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 
 import java.time.LocalDate;
