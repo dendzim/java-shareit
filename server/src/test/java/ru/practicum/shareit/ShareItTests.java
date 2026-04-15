a32389c628e3e6d84c69e7a52ae2753ddc0822fc
Denis Panasyuk <den.dzim@yandex.ru>
2026-04-15 14:35:29 +0300

    тестирование

--- server/src/test/java/ru/practicum/shareit/ShareItTests.java
+++ server/src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,8 +2,11 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.exceptions.ErrorHandler;
 import ru.practicum.shareit.exceptions.ErrorResponse;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
 
 @SpringBootTest
 class ShareItTests {
@@ -13,6 +16,9 @@ class ShareItTests {
 		ShareItApp.main(new String[]{});
 		new ErrorHandler();
 		new ErrorResponse("test");
+		new User();
+		new Item();
+		new Booking();
 	}
 
 }
